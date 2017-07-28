69f25893511f466813605aa31f033f529487dda8
Derek Oh <derek.oh93@gmail.com>
2017-07-28 13:45:17 -0700

    map

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the capstoneApp
  */
 var app = angular.module('capstoneApp', ['ngMap']);
-  app.controller('MainCtrl', function(ngMap, $scope) {
+  app.controller('MainCtrl', function($scope,ngMap) {
   $scope.googleMapsUrl='https://maps.googleapis.com/maps/api/js?key=AIzaSyCDtnE5ga16TGvvIM6i90-3BlucdIl4DMY';
   ngMap.getMap().then(function(map) {
     console.log(map.getCenter());
