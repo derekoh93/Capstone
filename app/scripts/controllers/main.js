5a9f0491661e5d35ad201cf852a3bd1a8e078642
Derek Oh <derek.oh93@gmail.com>
2017-08-02 14:56:40 -0700

    map implemented

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,12 @@
  * Controller of the capstoneApp
  */
 angular.module('capstoneApp')
-  .controller('MainCtrl', function($scope,ngMap) {
-   $scope.googleMapsUrl='https://maps.googleapis.com/maps/api/js?key=AIzaSyCDtnE5ga16TGvvIM6i90-3BlucdIl4DMY';
-   ngMap.getMap().then(function(map) {
+  .controller('MainCtrl', function($scope,NgMap) {
+  $scope.googleMapsUrl='https://maps.googleapis.com/maps/api/js?key=AIzaSyCDtnE5ga16TGvvIM6i90-3BlucdIl4DMY';
+   NgMap.getMap().then(function(map) {
      console.log(map.getCenter());
      console.log('markers', map.markers);
      console.log('shapes', map.shapes);
+
    });
  });
