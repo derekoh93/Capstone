c2c2ab98e8604a391dac40141130ca4535f9793c
Derek Oh <derek.oh93@gmail.com>
2017-08-16 15:05:37 -0700

    new map with search

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,23 @@
  * Controller of the capstoneApp
  */
  angular.module('capstoneApp')
-   .controller('MainCtrl', function($scope,NgMap) {
-   $scope.googleMapsUrl='https://maps.googleapis.com/maps/api/js?key=AIzaSyCDtnE5ga16TGvvIM6i90-3BlucdIl4DMY';
-    NgMap.getMap().then(function(map) {
-      console.log(map.getCenter());
-      console.log('markers', map.markers);
-      console.log('shapes', map.shapes);
-    });
+ .controller('MainCtrl', ['$scope',
+     function ($scope) {
 
-  });
+         $scope.center = [47.6062, -122.3321];
+         $scope.latlng = [47.6062, -122.3321];
+         $scope.getpos = function (event) {
+             $scope.lat = event.latLng.lat();
+             $scope.lng = event.latLng.lng();
+             $scope.latlng = [event.latLng.lat(), event.latLng.lng()];
+         };
+
+
+         $scope.placeMarker = function(){
+             console.log(this.getPlace());
+             var loc = this.getPlace().geometry.location;
+             $scope.latlng = [loc.lat(), loc.lng()];
+             $scope.center = [loc.lat(), loc.lng()];
+         };
+
+     }]);
